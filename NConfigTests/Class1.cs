dfe526c9cd491482e0950d0c9f7f3391823bfbe9
Avi Levi <avilevi3@gmail.com>
2011-06-13 17:44:49 +0200

    renamed tests project

---
+++ NConfigTests/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NConfig;
+using NConfig.Model;
+
+namespace TestProject1
+{
+
+    static class Items
+    {
+        /// <summary>
+        /// inside client installation folder.
+        /// at development time, with self hosting.
+        /// </summary>
+        public static ParameterValue ReletiveOneLevelDownPath = new ParameterValue(@".\logs" );
+        /// <summary>
+        /// must be absolute, because installation path is not known.
+        /// </summary>
+        public static ParameterValue applicationServerPath = new ParameterValue (@"c:\logs\");
+
+        public static ParameterValue prodOnlineClient = new ParameterValue (@"c:\UserData\ApplicationName\logs\" );
+        
+
+        /// <summary>
+        /// integration server logs to a different path?
+        /// </summary>
+        public static ParameterValue IntegrationServerPath = new ParameterValue (@"\someNetworDrive\integrationLogs\");
+
+        /// <summary>
+        /// specific service logs to a specific path?
+        /// </summary>
+        public static ParameterValue AuditServicePath = new ParameterValue(@"\\someNetworkDrive\enterpriseAuditData\");
+
+        public static ParameterValue[] All = new ParameterValue[] 
+        { ReletiveOneLevelDownPath, applicationServerPath, prodOnlineClient, IntegrationServerPath, AuditServicePath };
+    }
+    class env
+    {
+        public const string Name = "env";
+
+        public const string dev = "dev";
+        public const string prod = "prod";
+    }
+    class appType
+    {
+        public const string Name = "appType";
+
+        public const string onlineClient = "onlineClient";
+        public const string applicationServer = "applicationServer";
+        public const string integrationServer = "integrationServer";
+    }
+
+    class services
+    {
+        public const string name = "services";
+
+        public const string AuditService = "AuditService";
+        public const string SomeService = "SomeService";
+    }
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class RuntimeContextitemAttribute : Attribute
+    {
+        public RuntimeContextitemAttribute(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class FilterPolicyAttribute : Attribute
+    {
+        public FilterPolicyAttribute(Type policyType)
+        {
+            this.PolicyType = policyType;
+        }
+
+        public Type PolicyType { get; set; }
+    }
+
+
+}
---
+++ NConfigTests/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NConfig;
+using NConfig.Model;
+
+namespace TestProject1
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        class TestSection
+        {
+            public int Num { get; set; }
+            public string Name { get; set; }
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IConfigurationService svc = 
+            ConfigurationServiceBuilder.Instance
+                .SetRuntimeContext(()=>
+                    {
+                        return new Dictionary<string,string>
+                        {
+                            {"environment","development"},
+                            {"appType","onlineServer"}
+                        };
+                    })
+
+                .AddConfigurationData(sec =>
+                {
+                    sec.FromType<TestSection>()
+                        .AddParameter(new Parameter().FromExpression<TestSection>( x => x.Num)
+                            .AddValue(new ParameterValue("1")
+                                .WithReference("environment", "development")
+                                .WithReference("appType", "onlineServer"))
+                            .AddValue(new ParameterValue("2")
+                                .WithReference("environment", "production")
+                                .WithReference("appType", "onlineClient")));
+                }).Build();
+
+            TestSection section=svc.GetSection<TestSection>();
+            Assert.IsNotNull(section);
+        }
+    }
+}
