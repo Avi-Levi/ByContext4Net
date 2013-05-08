0e4de6f38dfbf0842d07269a10dbe329c6e53a19
Schmulik Raskin <schmuli@gmail.com>
2013-05-08 12:24:01 +0300

    Added JSON Tranlator

---
+++ NConfig.JsonTranslator/JsonTranslatorProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NConfig.StringToValueTranslator;
+
+namespace NConfig.JsonTranslators
+{
+    class JsonTranslatorProvider : IStringToValueTranslatorProvider
+    {
+        public const string ProviderKey = "Json";
+
+        public IStringToValueTranslator Get(Type type)
+        {
+            return new JsonTranslator(type);
+        }
+    }
+}
